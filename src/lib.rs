115e005eca7d63399177efc3c187f8bb6f2b6d98
revol-xut <revol-xut@protonmail.com>
2022-07-07 20:40:27 +0200

    extended schema

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,138 @@
+use chrono::prelude::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use serde::{
-    Serialize, 
-    Deserialize,
-};
-
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum R09Types {
     R14 = 14,
     R16 = 16,
-    R18 = 18
+    R18 = 18,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TelegramType {
+    PreRegistration = 0,
+    Registration = 1,
+    DeRegistration = 2,
+    DoorClosed = 3,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub struct Stop {
-    #[serde(alias = "DHID")] 
-    dhid: Option<String>,
-    name: Option<String>,
-    telegram_type: R09Types,
-    direction: u8,
-    lan: f64,
-    lon: f64
+pub struct TransmissionPosition {
+    #[serde(alias = "DHID")]
+    pub dhid: Option<String>,
+    pub name: Option<String>,
+    pub telegram_type: TelegramType,
+    pub direction: u8,
+    pub lat: f64,
+    pub lon: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct RegionMetaInformation {
-    frequency: Option<u64>,
-    city_name: Option<String>,
-    type_r09: Option<R09Types>
+    pub frequency: Option<u64>,
+    pub city_name: Option<String>,
+    pub type_r09: Option<R09Types>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct DocuemtnMetaInformation {
+    pub schema_version: String,
+    pub date: DateTime<Utc>,
+    pub generator: Option<String>,
+    pub generator_version: Option<String>,
+}
+
+pub type RegionalTransmissionPositions = HashMap<u32, Vec<TransmissionPosition>>;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct InterRegional {
+    pub document: DocuemtnMetaInformation,
+    pub data: HashMap<u64, RegionalTransmissionPositions>,
+    pub meta: HashMap<u64, RegionMetaInformation>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Region {
-    #[serde(flatten)]
-    stops: HashMap<u32, Vec<Stop>>,
-
-    meta: RegionMetaInformation
+    pub traffic_lights: RegionalTransmissionPositions,
+    pub meta: RegionMetaInformation,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub struct Stops(HashMap<u64, Region>);
+impl InterRegional {
+    pub fn from(file: &str) -> InterRegional {
+        let data = fs::read_to_string(file).expect("Unable to read file");
+        let res: InterRegional = serde_json::from_str(&data).expect("Unable to parse");
+        return res;
+    }
 
+    pub fn write(&self, file: &str) {
+        let json_data = serde_json::to_string_pretty(&self).unwrap();
+        let mut output = File::create(file).unwrap();
+        output.write_all(json_data.as_bytes()).unwrap();
+    }
 
-pub fn parse_from_file(file: &str) -> Stops {
-    let data = fs::read_to_string(file).expect("Unable to read file");
-    let res: Stops = serde_json::from_str(&data).expect("Unable to parse");
-    return res;
+    pub fn extract(&self, region: &u64) -> Option<Region> {
+        let data = self.data.get(region);
+        let meta = self.meta.get(region);
+
+        if data.is_none() || meta.is_none() {
+            return None;
+        }
+
+        Some(Region {
+            traffic_lights: data.unwrap().clone(),
+            meta: meta.unwrap().clone(),
+        })
+    }
+
+    pub fn look_up(
+        &self,
+        region_id: &u64,
+        traffic_light: &u32,
+    ) -> Option<Vec<TransmissionPosition>> {
+        match self.data.get(region_id) {
+            Some(region) => {
+                return region.get(traffic_light).map(|x| x.clone());
+            }
+            None => None,
+        }
+    }
+
+    pub fn get_approximate_position(
+        &self,
+        region_id: &u64,
+        traffic_light: &u32,
+    ) -> Option<TransmissionPosition> {
+        let stop_list = self.look_up(region_id, traffic_light);
+
+        match stop_list {
+            Some(possbile_stations) => {
+                if possbile_stations.len() == 0 {
+                    return None;
+                }
+
+                let selected_position = possbile_stations[0].clone();
+
+                for position in possbile_stations {
+                    if position.telegram_type == TelegramType::DoorClosed {
+                        return Some(position);
+                    }
+                }
+
+                Some(selected_position.clone())
+            }
+            None => None,
+        }
+    }
 }
 
 impl<'de> serde::Deserialize<'de> for R09Types {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: serde::Deserializer<'de>
+        D: serde::Deserializer<'de>,
     {
         struct R09TypesVisitor;
 
@@ -74,9 +154,9 @@ impl<'de> serde::Deserialize<'de> for R09Types {
 
             fn visit_u64<E: serde::de::Error>(self, n: u64) -> Result<R09Types, E> {
                 Ok(match n {
-                    1 => R09Types::R14,
-                    2 => R09Types::R16,
-                    3 => R09Types::R18,
+                    14 => R09Types::R14,
+                    16 => R09Types::R16,
+                    18 => R09Types::R18,
                     _ => return Err(E::invalid_value(serde::de::Unexpected::Unsigned(n), &self)),
                 })
             }
@@ -87,44 +167,69 @@ impl<'de> serde::Deserialize<'de> for R09Types {
 }
 
 impl Serialize for R09Types {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-            where S: ::serde::Serializer,
-        {
-            // Serialize the enum as a string.
-            serializer.serialize_str(match *self {
-                R09Types::R14 => "R09.14",
-                R09Types::R16 => "R09.16",
-                R09Types::R18 => "R09.18",
-            })
-        }
-}
-
-/*
-{
-
-    "14625020": { // ZHV (aka Germany-wide) gemeindeschlüssel
-        "1234": [ // traffic light ID
-            {
-                "telegram_type": 0, // possible values: pre_registration (0), registration (1), de_registration (2), doors_close (3)
-                "direction": 1, // direction identifier
-                "lat": 54.88141,
-                "lon": 8.291386
-            },
-            {
-                "telegram_type": 3, // doors close actually means we are most probably at a stop
-                "direction": 1,
-                "lat": 54.881241,
-                "lon": 8.29131,
-                "DHID": "de:01001:104053", // OPTIONAL Stop ID (ZHV, Germany-wide)
-                "name": "Katzenstr." // OPTIONAL
-            }
-        ],
-        "meta": {
-            "frequency": 105200000,
-            "city_name": "Westerland",
-            "telegram_format": "R09.18"
-        }
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ::serde::Serializer,
+    {
+        // Serialize the enum as a string.
+        serializer.serialize_str(match *self {
+            R09Types::R14 => "R09.14",
+            R09Types::R16 => "R09.16",
+            R09Types::R18 => "R09.18",
+        })
     }
 }
-*/
 
+impl<'de> serde::Deserialize<'de> for TelegramType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct TelegramTypeVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for TelegramTypeVisitor {
+            type Value = TelegramType;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "an integer or string representing a R09Type")
+            }
+
+            fn visit_str<E: serde::de::Error>(self, s: &str) -> Result<TelegramType, E> {
+                Ok(match s {
+                    "pre_registration" => TelegramType::PreRegistration,
+                    "registration" => TelegramType::Registration,
+                    "de_registration" => TelegramType::DeRegistration,
+                    "door_close" => TelegramType::DoorClosed,
+                    _ => return Err(E::invalid_value(serde::de::Unexpected::Str(s), &self)),
+                })
+            }
+
+            fn visit_u64<E: serde::de::Error>(self, n: u64) -> Result<TelegramType, E> {
+                Ok(match n {
+                    0 => TelegramType::PreRegistration,
+                    1 => TelegramType::Registration,
+                    2 => TelegramType::DeRegistration,
+                    3 => TelegramType::DoorClosed,
+                    _ => return Err(E::invalid_value(serde::de::Unexpected::Unsigned(n), &self)),
+                })
+            }
+        }
+
+        deserializer.deserialize_any(TelegramTypeVisitor)
+    }
+}
+
+impl Serialize for TelegramType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ::serde::Serializer,
+    {
+        // Serialize the enum as a string.
+        serializer.serialize_str(match *self {
+            TelegramType::PreRegistration => "0",
+            TelegramType::Registration => "1",
+            TelegramType::DeRegistration => "2",
+            TelegramType::DoorClosed => "3",
+        })
+    }
+}
